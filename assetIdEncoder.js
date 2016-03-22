fb330a50f42c7acc5c142e7c1d478dc4abb5475c
Oded Leiba <oded@colu.co>
2016-03-22 13:54:32 +0200

    aggregration policy prefixes

--- assetIdEncoder.js
+++ assetIdEncoder.js
@@ -1,37 +1,32 @@
-var cs = require('coinstring')
+var bs58check = require('bs58check')
 var hash = require('crypto-hashing')
-var UNLOCKEPADDING = 0xc8
-var LOCKEPADDING = 0x8e
-var NETWORKVERSIONS = [0x00, 0x05, 0x6f, 0xc4]
+var UNLOCKEPADDING = {
+  aggregatable: 0x2e37,
+  hybrid: 0x2e6b,
+  dispersed: 0x2e4e
+}
+var LOCKEPADDING = {
+  aggregatable: 0x20ce,
+  hybrid: 0x2102,
+  dispersed: 0x20e4
+}
 var POSTFIXBYTELENGTH = 2
 
 var padLeadingZeros = function (hex, byteSize) {
+  if (!byteSize) {
+    byteSize = Math.ceil(hex.length / 2)
+  }
   return (hex.length === byteSize * 2) ? hex : padLeadingZeros('0' + hex, byteSize)
 }
 
 var createId = function (publicKey, padding, divisibility) {
   divisibility = divisibility || 0
   divisibility = new Buffer(padLeadingZeros(divisibility.toString(16), POSTFIXBYTELENGTH), 'hex')
-  if (!Buffer.isBuffer(publicKey)) {
-    publicKey = new Buffer(publicKey, 'hex')
-  }
+  padding = new Buffer(padLeadingZeros(padding.toString(16)), 'hex')
   var hash256 = hash.sha256(publicKey)
   var hash160 = hash.ripemd160(hash256)
-  var hash160Buf = new Buffer(hash160, 'hex')
-  hash160Buf = Buffer.concat([hash160Buf, divisibility])
-  return cs.encode(hash160Buf, padding)
-}
-
-var createIdFromAddress = function (address, padding, divisibility) {
-  divisibility = divisibility || 0
-  divisibility = new Buffer(padLeadingZeros(divisibility.toString(16), POSTFIXBYTELENGTH), 'hex')
-  address = cs.decode(address)
-  var version = address.slice(0, 1)
-  // if (version[0] === 4) version = address.slice(0, 4)
-  var hash160Buf = address.slice(version.length, 21)
-  if (NETWORKVERSIONS.indexOf(parseInt(version.toString('hex'), 16)) === -1) throw new Error('Unrecognized address network')
-  hash160Buf = Buffer.concat([hash160Buf, divisibility])
-  return cs.encode(hash160Buf, padding)
+  var concatenation = Buffer.concat([padding, hash160, divisibility])
+  return bs58check.encode(concatenation)
 }
 
 module.exports = function (bitcoinTransaction) {
@@ -39,8 +34,9 @@ module.exports = function (bitcoinTransaction) {
   if (bitcoinTransaction.cc_data[0].type !== 'issuance') throw new Error('Not An issuance transaction')
   if (typeof bitcoinTransaction.cc_data[0].lockStatus === 'undefined') throw new Error('Missing Lock Status data')
   var lockStatus = bitcoinTransaction.cc_data[0].lockStatus
+  var aggregationPolicy = bitcoinTransaction.cc_data[0].aggregationPolicy
+  var divisibility = bitcoinTransaction.cc_data[0].divisibility
   var firstInput = bitcoinTransaction.vin[0]
-  if (lockStatus) return createId(firstInput.txid + '-' + firstInput.vout, LOCKEPADDING, bitcoinTransaction.cc_data[0].divisibility)
-  if (firstInput.scriptSig && firstInput.scriptSig.asm) return createId(firstInput.scriptSig.asm.split(' ')[1], UNLOCKEPADDING, bitcoinTransaction.cc_data[0].divisibility)
-  if (firstInput.address) return createIdFromAddress(firstInput.address, UNLOCKEPADDING, bitcoinTransaction.cc_data[0].divisibility)
+  if (lockStatus) return createId(firstInput.txid + '-' + firstInput.vout, LOCKEPADDING[aggregationPolicy], divisibility)
+  if (firstInput.scriptSig && firstInput.scriptSig.asm) return createId(firstInput.scriptSig.asm.split(' ')[1], UNLOCKEPADDING[aggregationPolicy], divisibility)
 }
--- test/test.js
+++ test/test.js
@@ -1,4 +1,6 @@
-var assetId = require(__dirname + '/../assetIdEncoder')
+/* eslint-env mocha */
+var path = require('path')
+var assetId = require(path.join(__dirname, '/../assetIdEncoder'))
 var assert = require('assert')
 var assetId1, assetId2, assetId3
 
@@ -10,6 +12,7 @@ describe('Test Issuance decoder', function () {
        'version': 1,
        'type': 'issuance',
        'lockStatus': false,
+       'aggregationPolicy': 'aggregatable',
        'divisibility': 3,
        'amount': 1,
        'payments': [{
@@ -69,135 +72,69 @@ describe('Test Issuance decoder', function () {
    'version': 1,
    'hex': '01000000014f95ad5927d534ee4edb405e1616c7b3a4f8f5f3e06772873183cd8b8af3450f020000006b483045022100daf8f8d65ea908a28d90f700dc932ecb3b68f402b04ba92f987e8abd7080fcad02205ce81b698b8013b86813c9edafc9e79997610626c9dd1bfb49f60abee9daa4380121029b622e5f0f87f2be9f23c4d82f818a73e258a11c26f01f73c8b595042507a574ffffffff0358020000000000001976a9145c7432e747af0d7e8f60de97d5ddd30ec1d9c72688ac00000000000000000a6a084343010501000100808c0d04000000001976a914b8df2b3d4ca896915875afeda7730be816559df688ac00000000'
   }
-  it('should return Unlocked asset ID from scriptSig.asm', function (done) {
+  it('should return Unlocked aggregatableasset ID from scriptSig.asm', function (done) {
     assetId1 = assetId(bitcoinTransaction)
-    assert.equal(assetId1[0], 'U', 'Should be Unlocked')
-    // console.log(assetId(bitcoinTransaction))
+    assert.equal(assetId1.startsWith('Ua'), true, 'Should be Unlocked and aggregatable')
+    console.log(assetId(bitcoinTransaction))
     done()
   })
 
-  it('should return Locked asset ID', function (done) {
+  it('should return Unlocked hybrid asset ID from scriptSig.asm', function (done) {
+    bitcoinTransaction.cc_data[0].aggregationPolicy = 'hybrid'
+    assetId1 = assetId(bitcoinTransaction)
+    assert.equal(assetId1.startsWith('Uh'), true, 'Should be Unlocked and hybrid')
+    console.log(assetId(bitcoinTransaction))
+    done()
+  })
+
+  it('should return Unlocked dispersed asset ID from scriptSig.asm', function (done) {
+    bitcoinTransaction.cc_data[0].aggregationPolicy = 'dispersed'
+    assetId1 = assetId(bitcoinTransaction)
+    assert.equal(assetId1.startsWith('Ud'), true, 'Should be Unlocked and dispersed')
+    console.log(assetId(bitcoinTransaction))
+    done()
+  })
+
+  it('should return Locked aggregatable asset ID', function (done) {
     bitcoinTransaction.cc_data[0].lockStatus = true
+    bitcoinTransaction.cc_data[0].aggregationPolicy = 'aggregatable'
     assetId2 = assetId(bitcoinTransaction)
-    assert.equal(assetId2[0], 'L', 'Should be Locked')
-    // console.log(assetId(bitcoinTransaction))
+    assert.equal(assetId2.startsWith('La'), true, 'Should be Locked and aggregatable')
+    console.log(assetId(bitcoinTransaction))
     done()
   })
 
-  it('should return Unlocked asset ID from testnet address', function (done) {
-    bitcoinTransaction = {
-      cc_data:
-        [{
-          type: 'issuance',
-          lockStatus: false,
-          divisibility: 3
-        }],
-      vin:
-      [{ txid: '095d3352d3c54b435d833be5d78016e3daa49b137a20c2941ed80214b519ecbe',
-        vout: 2,
-        address: 'mxNTyQ3WdFMQE7SGVpSQGXnSDevGMLq7dg'
-      }]
-    }
-    assetId3 = assetId(bitcoinTransaction)
-    assert.equal(assetId3[0], 'U', 'Should be Unlocked')
-    assert.equal(assetId3, assetId1, 'Should get the same assetId from public key or asset')
-    // console.log(assetId(bitcoinTransaction))
+  it('should return Locked hybrid asset ID', function (done) {
+    bitcoinTransaction.cc_data[0].aggregationPolicy = 'hybrid'
+    assetId2 = assetId(bitcoinTransaction)
+    assert.equal(assetId2.startsWith('Lh'), true, 'Should be Locked and hybrid')
+    console.log(assetId(bitcoinTransaction))
     done()
   })
 
-  it('should return Unrecognized address network Error', function (done) {
-    bitcoinTransaction = {
-      cc_data:
-        [{
-          type: 'issuance',
-          lockStatus: false
-        }],
-      vin:
-      [{ txid: '095d3352d3c54b435d833be5d78016e3daa49b137a20c2941ed80214b519ecbe',
-        vout: 2,
-        address: 'fxNTyQ3WdFMQE7SGVpSQGXnSDevGMLq7dg'
-      }]
-    }
-    assert.throws(function () {
-      assetId(bitcoinTransaction)
-    }
-    , 'Unrecognized address network'
-    , 'Unrecognized address network')
-    done()
-  })
-
-  it('should return Unlocked asset ID from mainnet address', function (done) {
-    bitcoinTransaction = {
-      cc_data:
-        [{
-          type: 'issuance',
-          lockStatus: false
-        }],
-      vin:
-      [{ txid: '095d3352d3c54b435d833be5d78016e3daa49b137a20c2941ed80214b519ecbe',
-        vout: 2,
-        address: '1PuKhp9CmFL9Xs2apKKeTAtLoZPcvoikE1'
-      }]
-    }
-    assetId3 = assetId(bitcoinTransaction)
-    assert.equal(assetId3[0], 'U', 'Should be Unlocked')
-    // console.log(assetId3)
-    done()
-  })
-
-  it('should return Unlocked asset ID from mainnet script address', function (done) {
-    bitcoinTransaction = {
-      cc_data:
-        [{
-          type: 'issuance',
-          lockStatus: false
-        }],
-      vin:
-      [{ txid: '095d3352d3c54b435d833be5d78016e3daa49b137a20c2941ed80214b519ecbe',
-        vout: 2,
-        address: '3EktnHQD7RiAE6uzMj2ZifT9YgRrkSgzQX'
-      }]
-    }
-    assetId3 = assetId(bitcoinTransaction)
-    assert.equal(assetId3[0], 'U', 'Should be Unlocked')
-    // console.log(assetId3)
+  it('should return Locked dispersed asset ID', function (done) {
+    bitcoinTransaction.cc_data[0].aggregationPolicy = 'dispersed'
+    assetId2 = assetId(bitcoinTransaction)
+    assert.equal(assetId2.startsWith('Ld'), true, 'Should be Locked and dispersed')
+    console.log(assetId(bitcoinTransaction))
     done()
   })
 
   it('should return Locked asset ID', function (done) {
     bitcoinTransaction = {
-       'cc_data': [{
-         'type': 'issuance',
-         'lockStatus': true
-       }],
-       'vin': [{
-         'txid': '0f45f38a8bcd8331877267e0f3f5f8a4b3c716165e40db4eee34d52759ad954f',
-         'vout': 2
-       }]
-     }
-    assetId3 = assetId(bitcoinTransaction)
-    assert.equal(assetId3[0], 'L', 'Should be Locked')
-    // console.log(assetId3)
-    done()
-  })
-
-  it('should return Unlocked asset ID from testnet script address', function (done) {
-    bitcoinTransaction = {
-      cc_data:
-        [{
-          type: 'issuance',
-          lockStatus: false
-        }],
-      vin:
-      [{ txid: '095d3352d3c54b435d833be5d78016e3daa49b137a20c2941ed80214b519ecbe',
-        vout: 2,
-        address: '2MzQwSSnBHWHqSAqtTVQ6v47XtaisrJa1Vc'
+      cc_data: [{
+        type: 'issuance',
+        aggregationPolicy: 'aggregatable',
+        lockStatus: true
+      }],
+      vin: [{
+        txid: '0f45f38a8bcd8331877267e0f3f5f8a4b3c716165e40db4eee34d52759ad954f',
+        vout: 2
       }]
     }
     assetId3 = assetId(bitcoinTransaction)
-    assert.equal(assetId3[0], 'U', 'Should be Unlocked')
-    // console.log(assetId3)
+    assert.equal(assetId3.startsWith('La'), true, 'Should be Locked and aggregatable')
+    console.log(assetId3)
     done()
   })
-
 })
