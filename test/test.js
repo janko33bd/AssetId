31ab98b43f2b0f62ceb40382427f9112b5b43582
Oded Leiba <oded@colu.co>
2016-03-23 16:16:23 +0200

    test exact ids match

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ var assetId1, assetId2, assetId3
 describe('Test Issuance decoder', function () {
   var bitcoinTransaction = {
    'txid': 'bc1083ff98873050d2fa9d38823057f0125161ecad43ab2e2dcd1dd3c2668fb7',
-   'cc_data': [{
+   'ccdata': [{
        'protocol': 17219,
        'version': 1,
        'type': 'issuance',
@@ -74,55 +74,55 @@ describe('Test Issuance decoder', function () {
   }
   it('should return Unlocked aggregatableasset ID from scriptSig.asm', function (done) {
     assetId1 = assetId(bitcoinTransaction)
-    assert.equal(assetId1.startsWith('Ua'), true, 'Should be Unlocked and aggregatable')
-    console.log(assetId(bitcoinTransaction))
+    assert.equal(assetId1, 'Ua6d5eyBvWE8oros8mBtMoyjWhzeopVJ9aEfyB', 'Should be Unlocked and aggregatable')
+    console.log(assetId1)
     done()
   })
 
   it('should return Unlocked hybrid asset ID from scriptSig.asm', function (done) {
-    bitcoinTransaction.cc_data[0].aggregationPolicy = 'hybrid'
+    bitcoinTransaction.ccdata[0].aggregationPolicy = 'hybrid'
     assetId1 = assetId(bitcoinTransaction)
-    assert.equal(assetId1.startsWith('Uh'), true, 'Should be Unlocked and hybrid')
-    console.log(assetId(bitcoinTransaction))
+    assert.equal(assetId1, 'Uh8FS1idiZCDcg8bsCW1CPqN8CfnuzjbaXEXGn', 'Should be Unlocked and hybrid')
+    console.log(assetId1)
     done()
   })
 
   it('should return Unlocked dispersed asset ID from scriptSig.asm', function (done) {
-    bitcoinTransaction.cc_data[0].aggregationPolicy = 'dispersed'
+    bitcoinTransaction.ccdata[0].aggregationPolicy = 'dispersed'
     assetId1 = assetId(bitcoinTransaction)
-    assert.equal(assetId1.startsWith('Ud'), true, 'Should be Unlocked and dispersed')
-    console.log(assetId(bitcoinTransaction))
+    assert.equal(assetId1, 'UdCvGtcFXNciAZpLxXr1qzVVbFe3MYwhUg55vA', 'Should be Unlocked and dispersed')
+    console.log(assetId1)
     done()
   })
 
   it('should return Locked aggregatable asset ID', function (done) {
-    bitcoinTransaction.cc_data[0].lockStatus = true
-    bitcoinTransaction.cc_data[0].aggregationPolicy = 'aggregatable'
+    bitcoinTransaction.ccdata[0].lockStatus = true
+    bitcoinTransaction.ccdata[0].aggregationPolicy = 'aggregatable'
     assetId2 = assetId(bitcoinTransaction)
-    assert.equal(assetId2.startsWith('La'), true, 'Should be Locked and aggregatable')
-    console.log(assetId(bitcoinTransaction))
+    assert.equal(assetId2, 'La8VDdCe7uWoL1igL5guQq2v2wf68ViJiK9gFe', 'Should be Locked and aggregatable')
+    console.log(assetId2)
     done()
   })
 
   it('should return Locked hybrid asset ID', function (done) {
-    bitcoinTransaction.cc_data[0].aggregationPolicy = 'hybrid'
+    bitcoinTransaction.ccdata[0].aggregationPolicy = 'hybrid'
     assetId2 = assetId(bitcoinTransaction)
-    assert.equal(assetId2.startsWith('Lh'), true, 'Should be Locked and hybrid')
-    console.log(assetId(bitcoinTransaction))
+    assert.equal(assetId2, 'LhA7Zyx5uxUt8q3R4X12FQtYeSLEEfxc9chGYo', 'Should be Locked and hybrid')
+    console.log(assetId2)
     done()
   })
 
   it('should return Locked dispersed asset ID', function (done) {
-    bitcoinTransaction.cc_data[0].aggregationPolicy = 'dispersed'
+    bitcoinTransaction.ccdata[0].aggregationPolicy = 'dispersed'
     assetId2 = assetId(bitcoinTransaction)
-    assert.equal(assetId2.startsWith('Ld'), true, 'Should be Locked and dispersed')
-    console.log(assetId(bitcoinTransaction))
+    assert.equal(assetId2, 'Ld6wkYbUxPYDfpWXe31ivJjzNRQRB7H8Jx7tYT', 'Should be Locked and dispersed')
+    console.log(assetId2)
     done()
   })
 
   it('should return Locked asset ID', function (done) {
     bitcoinTransaction = {
-      cc_data: [{
+      ccdata: [{
         type: 'issuance',
         aggregationPolicy: 'aggregatable',
         lockStatus: true
@@ -133,7 +133,7 @@ describe('Test Issuance decoder', function () {
       }]
     }
     assetId3 = assetId(bitcoinTransaction)
-    assert.equal(assetId3.startsWith('La'), true, 'Should be Locked and aggregatable')
+    assert.equal(assetId3, 'La8VDdCe7uWoL1igL5guQq2v2wf68ViJR8dpDe', 'Should be Locked and aggregatable')
     console.log(assetId3)
     done()
   })
